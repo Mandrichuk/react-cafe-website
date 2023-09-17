b7b06aaae246aad93520ccc53d530e327b229c28
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-09-17 21:51:42 +0200

    Finalize main page

--- src/scripts/cafe/CreateMain.js
+++ src/scripts/cafe/CreateMain.js
@@ -31,26 +31,30 @@ export default function CreateMain() {
         <div className="circules-container">
 
           <div className="white-circle-container white-circle-container-1">
-            <img className="product-image product-image-1" src="images/decorations/cheeseburger.jpg"/>
+            <img className="product-image product-image-1" src="images/decorations/hamburger.png"/>
           </div>
 
           <div className="white-circle-container white-circle-container-2">
-            <img className="product-image product-image-2" src="images/decorations/beer-glass.jpg"/>
+            <img className="product-image product-image-2" src="images/decorations/beer-glass.png"/>
           </div>
 
           <div className="white-circle-container white-circle-container-3">
-            <img className="product-image product-image-3" src="images/decorations/donut.jpg"/>
+            <img className="product-image product-image-3" src="images/decorations/donut.png"/>
           </div>
 
           <div className="white-circle-container white-circle-container-4">
-            <img className="product-image product-image-4" src="images/decorations/hotdog.jpg"/>
+            <img className="product-image product-image-4" src="images/decorations/hotdog.png"/>
+          </div>
+
+          <div className="white-circle-container white-circle-container-5">
+            <img className="product-image product-image-5" src="images/decorations/milkshake.png"/>
           </div>
         </div>
       </div>
 
       <div className="main-second-section">
 
-        <div className="info-container">
+        <div className="info-separation-container">
         
         </div>
 
@@ -91,10 +95,88 @@ export default function CreateMain() {
             </div>
         </div>
 
-        {/* <div className="location-container">
+        <div className="location-container">
           <img className="location-image" src="images/decorations/location.jpg" />
-          
-        </div> */}
+
+          <div className="cutaway-holder-container">
+
+            <div className="cutaway-container">
+              <div className="cutaway-info-container">
+
+                <div className="name-container">
+                  Drink&Food
+                </div>
+                <div className="tagline-container">
+                  Де люди приходять поїсти
+                </div>
+
+                <div className="contacts-location-container">
+                  <div className="location-text-container">
+                    Пр. Соборний 4-б
+                  </div>
+                  <div className="contacts-container">
+                    Номер
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="third-section-container">
+          <div className="nav-container">
+            <div className="nav__logo-container">separation-container
+              <img className="nav__logo-image" src="images/icons/brand-logo.png"/>
+            </div>
+
+            <div className="links-buttons-container">
+
+
+              <div className="nav__general-links">
+
+                <a href="#" className="nav__link nav__link-home">
+                  <div className="nav__link-container">
+                    Головна
+                  </div>
+                </a>
+                <a href="#" className="nav__link nav__link-about">
+                  <div className="nav__link-container">
+                    Про нас
+                  </div>
+                </a>
+                <a href="#" className="nav__link nav__link-menu">
+                  <div className="nav__link-container">
+                    Меню
+                  </div>
+                </a>
+                <a href="#" className="nav__link nav__link-location">
+                  <div className="nav__link-container">
+                    Локація
+                  </div>
+                </a>
+                <a href="#" className="nav__link nav__link-contacts">
+                  <div className="nav__link-container">
+                    Контакти
+                  </div>
+                </a>
+
+              </div>
+
+              <div className="sosial-buttons-container"> 
+                <a target="_blank" href="https://t.me/Drink_Food_zp">
+                  <button className="telegram-btn">
+                      Telegram
+                  </button>
+                </a>
+                <a target="_blank" href="https://www.instagram.com/drink.food.zp/?igshid=MzRlODBiNWFlZA%3D%3D">
+                  <button className="instagram-btn">Instagram</button>
+                </a>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
 
       </div>
       </div>

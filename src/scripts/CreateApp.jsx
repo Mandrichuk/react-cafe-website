492a9cb57b16af819532a054128236e435e98f5e
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-10-03 22:02:06 +0200

    Creating the page 'pathes'

--- src/scripts/CreateApp.jsx
+++ src/scripts/CreateApp.jsx
@@ -1,12 +1,18 @@
 import React, {useState} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Cafe from "../scripts/cafe/CreateCafe.jsx";
 import Menu from "../scripts/menu/CreateMenu.jsx";
 
 export default function CreateApp() {
   return (
-    <>
-      <Cafe />
-      {/* <Menu /> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Cafe />} />
+      </Routes>
+      <Routes> 
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </Router>
   );
 }

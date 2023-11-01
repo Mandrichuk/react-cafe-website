870cce6881956092e45d964935db28436b811cbd
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-11-01 16:40:32 +0100

    Making the 'Change' button work

--- src/super-admin/scripts/mealEdit/CreateMealEdit.jsx
+++ src/super-admin/scripts/mealEdit/CreateMealEdit.jsx
@@ -3,24 +3,46 @@ import styles from '../../styles/pages/mealEdit.module.css';
 import mealById from '../../../user/scripts/shared/getMeal';
 
 import { TbCategoryFilled, TbStatusChange } from "react-icons/tb";
-import { GiHotMeal, GiPriceTag } from "react-icons/gi";
+import { GiConsoleController, GiHotMeal, GiPriceTag } from "react-icons/gi";
 import { RiScales2Line } from "react-icons/ri";
 import { IoWaterOutline } from "react-icons/io5";
 import { AiOutlineDelete } from "react-icons/ai";
 import { BiUpload } from "react-icons/bi";
 
+import menuData from '../../../data/menu/menuData';
+import { current } from '@reduxjs/toolkit';
+
+
 export default function CreateMealEdit() {
+  const getAllCategoriesName = menuData.map(item => item.name);
   const currentPath = window.location.pathname;
   const pathSegments = currentPath.split('/');
   const lastSegment = Number(pathSegments[pathSegments.length - 1]);
+
   let currentMeal = mealById(lastSegment);
+  const currentCategory = menuData[currentMeal.categoryId - 1];
 
   const [editMode, setEditMode] = useState(false);
+  const [formData, setFormData] = useState({
+    catetory: currentMeal,
+    name: currentMeal.name,
+    capacity: currentMeal.grams,
+    units: currentMeal,
+    price: currentMeal.price,
+  });
 
-
-  function handleModeChange() {
-    setEditMode(editMode => !editMode);
+  function handleFormChange(event) {
+    setFormData((formData) => {
+      return {
+        ...formData,
+        [event.target.name]: event.target.value,
+      };
+    });
   }
+  
+  function handleModeChange() {setEditMode((prevEditMode) => !prevEditMode);}
+  function capitalize(str) {return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();}
+
 
   return (
     <div className={`${styles.mainContainer} flex flex-col items-center justify-between w-[100%] mt-[120px]`}>
@@ -29,7 +51,24 @@ export default function CreateMealEdit() {
         <div className={`${styles.specificationContainer}`}>
           <TbCategoryFilled className="text-custom-green mr-[10px]"/> Категорія
         </div>
-        <div className={`${styles.currentMealContainer}`}>Категорія</div>
+        {editMode ?
+            <section id="unitsSection" className={`${styles.unitsSection}`}>
+            <select 
+              onChange={handleFormChange} 
+              id="unitSelector" 
+              className={`${styles.unitsSelect}`}
+              name="catetory"
+            >
+            <option key={currentCategory.name} value={currentCategory.name}>{currentCategory.name}</option>
+            {getAllCategoriesName.map(categoryName => {
+              if (categoryName !== currentCategory.name) {
+                return <option key={categoryName} value={categoryName}>{categoryName}</option>
+              }
+              })}
+            </select>
+            </section>  :
+          <div className={`${styles.currentMealContainer}`}>{currentCategory.name}</div>
+        }
       </div>
 
       <div className={`${styles.categoryContainer}`}>
@@ -37,7 +76,17 @@ export default function CreateMealEdit() {
           <GiHotMeal className="text-custom-green mr-[10px]"/>
           Назва
         </div>
-        <div className={`${styles.currentMealContainer}`}>{currentMeal.name}</div>
+        {editMode ?
+          <input 
+          onChange={handleFormChange}
+          placeholder="Введіть назву"
+          className={`${styles.changeMealInput}`}
+          type="text"
+          name="name"
+          value={formData.name}
+          /> :
+          <div className={`${styles.currentMealContainer}`}>{currentMeal.name}</div> 
+        }
       </div>
 
       <div className={`${styles.categoryContainer}`}>
@@ -45,7 +94,17 @@ export default function CreateMealEdit() {
           <RiScales2Line className="text-custom-green mr-[10px]"/>
           Об'єм
         </div>
-        <div className={`${styles.currentMealContainer}`}>{currentMeal.grams}</div>
+        {editMode ?
+          <input 
+          onChange={handleFormChange}
+          placeholder="Введіть об'єм"
+          className={`${styles.changeMealInput}`}
+          type="text"
+          name="capacity"
+          value={currentMeal.capacity}
+          /> :
+          <div className={`${styles.currentMealContainer}`}>{currentMeal.capacity}</div> 
+        }
       </div>
 
       <div className={`${styles.categoryContainer}`}>
@@ -53,7 +112,23 @@ export default function CreateMealEdit() {
           <IoWaterOutline className="text-custom-green mr-[10px]"/>
           Одиниці виміру
         </div>
-        <div className={`${styles.currentMealContainer}`}>Одиниці виміру</div>
+        {editMode ?
+          <section id="unitsSection" className={`${styles.unitsSection}`}>
+            <select 
+              onChange={handleFormChange}
+              id="unitSelector" 
+              className={`${styles.unitsSelect}`}
+              name="units"
+              >
+              <option value="{currentMeal.units}">{capitalize(currentMeal.units)}</option>
+              {currentMeal.units === "гм" ?
+                <option value="мг">Гр</option> :
+                <option value="гм">Мг</option> 
+              } 
+            </select>
+          </section> :
+          <div className={`${styles.currentMealContainer}`}>Грами</div> 
+        }
       </div>
 
       <div className={`${styles.categoryContainer}`}>
@@ -61,26 +136,35 @@ export default function CreateMealEdit() {
           <GiPriceTag className="text-custom-green mr-[10px]"/>
           Ціна
         </div>
-        <div className={`${styles.currentMealContainer}`}>{currentMeal.price}</div>
+        {editMode ?
+          <input 
+          onChange={handleFormChange}
+          placeholder="Введіть ціну" 
+          className={`${styles.changeMealInput}`}
+          type="text"
+          name="price"
+          value={formData.price}
+          /> :
+          <div className={`${styles.currentMealContainer}`}>{currentMeal.price}</div> 
+        }
       </div>
 
       {!editMode &&
         <div className={`${styles.btnsContainer} flex flex-row items-center justify-center`}>
-            <button className={`${styles.btnActions} ${styles.changeBtn} flex flex-row items-center`}>
-              <TbStatusChange />
+            <button onClick={handleModeChange} className={`${styles.btnActions} ${styles.changeBtn} flex flex-row items-center`}>
+              <TbStatusChange className='mr-[10px]'/>
               Змінити
             </button>
         
             <button className={`${styles.btnActions} ${styles.deleteBtn} flex flex-row items-center`}>
-              <AiOutlineDelete />
+              <AiOutlineDelete className='mr-[10px]'/>
               Видалити
             </button>
         </div>
       }
-
       {editMode &&
-        <button  className={`${styles.btnActions} ${styles.saveBtn} flex flex-row items-center`}>
-          <BiUpload />
+        <button onClick={handleModeChange}  className={`${styles.btnActions} ${styles.saveBtn} flex flex-row items-center`}>
+          <BiUpload className='mr-[10px]'/>
           Зберегти
         </button>
       }
@@ -88,3 +172,4 @@ export default function CreateMealEdit() {
     </div>
   )
 }
+
